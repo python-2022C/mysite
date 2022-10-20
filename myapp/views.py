336e8e0d6f59354cfefe5a758a95c7b6288f89e6
ZARIFMAC <uzbzarif@gmail.com>
2022-10-20 18:54:54 +0500

    varibles

--- myapp/views.py
+++ myapp/views.py
@@ -9,7 +9,12 @@ def home(request):
         'age': 25,
         'email': 'john@gmail.com',
         'address': '123 Main St',
-        'phone': '555-555-5555'
+        'list': [1,2,3,4,5,6,7,8,9,10],
+        'products': {
+            'product1': 'Laptop',
+            'product2': 'Desktop',
+        }
+
     }
   
     return render(request, 'home.html',context=context)
