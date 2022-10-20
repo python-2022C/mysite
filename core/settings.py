e08f2209919193afa1943d50b4ebfcf0d9961b5a
ZARIFMAC <uzbzarif@gmail.com>
2022-10-20 17:43:29 +0500

    allowed

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-+*cy57!6uh%tr=$e&g)v9#zt0y4v49jr80r7q%7emilso3fj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
