2d1729c320ec39f64385aa5a1a1a24101943accb
ZARIFMAC <uzbzarif@gmail.com>
2022-10-20 18:41:28 +0500

    add templates

--- myapp/views.py
+++ myapp/views.py
@@ -3,16 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    HTML = """
-    <html>
-    <head>
-    <title>My App</title>
-    </head>
-    <body>
-    <h1>My App</h1>
-    <p>My App is a Django app.</p>
-    </body>
-    </html>
-    """
-    return HttpResponse(HTML)
-    # return render(request, 'home.html')
+  
+  
+    return render(request, 'home.html')
