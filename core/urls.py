a3841716e41acb64d281813f10b2275eff6a2522
ZARIFMAC <uzbzarif@gmail.com>
2022-10-20 18:35:18 +0500

    add home url

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from django.http import HttpResponse
 #import render
 from django.shortcuts import render
-
+from myapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 
 ]
