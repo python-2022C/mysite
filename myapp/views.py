d9caa35cc774403739f12c756afaa0e006887e90
ZARIFMAC <uzbzarif@gmail.com>
2022-10-20 18:47:34 +0500

    simple variables

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    context = {
+        'username': 'John',
+        'age': 25,
+        'email': 'john@gmail.com',
+        'address': '123 Main St',
+        'phone': '555-555-5555'
+    }
   
-  
-    return render(request, 'home.html')
+    return render(request, 'home.html',context=context)
